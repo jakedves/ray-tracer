ac9acf54d04ce802b679b4db442b92932ef242da
Jake <jakedves@gmail.com>
2023-07-08 23:46:28 +0100

    add ability to detect hitting a sphere

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,4 @@
-use cgmath::Vector3;
+use cgmath::{InnerSpace, Vector3};
 
 type Point = Vector3<f64>;
 
@@ -13,6 +13,23 @@ impl Ray {
     }
 
     pub fn at(&self, t: f64) -> Point {
-        return self.origin + t * self.direction;
+        self.origin + t * self.direction
+    }
+
+    /**
+     * ((x - c1)^2, (y - c2)^2, (z - c3)^2) = radius^2 is the formula for a sphere
+     *
+     * We are using some rearranged formula to solve a quadratic equation. We are looking
+     * to see if the intersection of the ray and there sphere, has any existing real roots,
+     * which happens when the discriminant of the quadratic equation is positive.
+     */
+    pub fn hits_sphere(&self, center: Point, radius: f64) -> bool {
+        let translation = self.origin - center;
+        let a = self.direction.dot(self.direction);
+        let b = 2.0 * translation.dot(self.direction);
+        let c = translation.dot(translation) - radius.powi(2);
+        let discriminant = b.powi(2) - 4.0 * a * c;
+
+        discriminant > 0.0
     }
 }
