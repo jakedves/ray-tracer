db21f2c0bf58dabb853576874ec9581c9219e2a9
Jake <jakedves@gmail.com>
2023-07-09 00:20:18 +0100

    use copy semantics

--- src/vector.rs
+++ src/vector.rs
@@ -3,14 +3,14 @@ use cgmath::Vector3;
 pub type Color = Vector3<f64>;
 pub type Point = Vector3<f64>;
 
-pub fn unit_vector(vector: &Point) -> Point {
+pub fn unit_vector(vector: Point) -> Point {
     vector / vector_length(vector)
 }
 
-pub fn vector_length(vector: &Point) -> f64 {
+pub fn vector_length(vector: Point) -> f64 {
     length_squared(vector).sqrt()
 }
 
-pub fn length_squared(vector: &Point) -> f64 {
+pub fn length_squared(vector: Point) -> f64 {
     vector.x * vector.x + vector.y * vector.y + vector.z * vector.z
 }
