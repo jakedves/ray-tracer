e7179463ffa346a9dcdccfe96567b550abe85f3a
Jake <jakedves@gmail.com>
2023-07-10 15:49:37 +0100

    dielectric material

--- src/material.rs
+++ src/material.rs
@@ -3,13 +3,18 @@ use cgmath::InnerSpace;
 use crate::{
     hittable::HitRecord,
     ray::Ray,
-    vector::{near_zero, random_in_unit_sphere, random_unit_vector, reflect, unit_vector, Color},
+    vector::{
+        near_zero, random_in_unit_sphere, random_unit_vector, reflect, refract, unit_vector, Color,
+    },
 };
+
+use rand::Rng;
 
 #[derive(Clone, Copy)]
 pub enum Material {
     Lambertian { albedo: Color },
     Metal { albedo: Color, fuzz: f64 },
+    Dielectric { refraction_index: f64 },
 }
 
 impl Material {
@@ -42,6 +47,39 @@ impl Material {
 
                 scattered.direction.dot(record.normal) > 0.0
             }
+
+            Material::Dielectric { refraction_index } => {
+                *attenuation = Color::new(1.0, 1.0, 1.0);
+                let refraction_ratio = if record.front_face {
+                    1.0 / refraction_index
+                } else {
+                    *refraction_index
+                };
+
+                let unit_direction = unit_vector(ray.direction);
+                let cos_theta = record.normal.dot(-unit_direction).min(1.0);
+                let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+                let cannot_refract = refraction_ratio * sin_theta > 1.0;
+                let mut rng = rand::thread_rng();
+
+                let direction =
+                    if cannot_refract || reflectance(cos_theta, refraction_ratio) > rng.gen() {
+                        reflect(unit_direction, record.normal)
+                    } else {
+                        refract(unit_direction, record.normal, refraction_ratio)
+                    };
+
+                *scattered = Ray::new(record.point, direction);
+
+                true
+            }
         }
     }
+}
+
+pub fn reflectance(cosine: f64, refractive_index: f64) -> f64 {
+    let r0 = ((1.0 - refractive_index) / (1.0 + refractive_index)).powi(2);
+
+    r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
